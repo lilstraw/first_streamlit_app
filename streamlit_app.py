ec8f8de71328e9d58f2db8a87756c0b35e9aca0e
lilstraw <121640040+lilstraw@users.noreply.github.com>
2022-12-29 21:21:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
-streamlit.header('My Parents New Healthy Dinner')
-streamlit.text('Some nonesense text')
-streamlit.text('Some more nonsese text ;)')
+streamlit.header('🥣 Breakfast Menu')  
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
